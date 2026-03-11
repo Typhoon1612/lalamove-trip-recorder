0bc5aa332bd9f1f512c9a8805383693594f10d84
Typhoon1612 <119401052+Typhoon1612@users.noreply.github.com>
2026-03-11 16:27:24 +0800

    Update files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="bg-white p-6 rounded-lg shadow-md border-t-4 border-orange-500">
-        <h2 class="text-2xl font-bold mb-4 text-gray-800">Record a New Trip</h2>
-        <p class="text-gray-600">This is where our awesome form will go very soon!</p>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
+        <div class="md:col-span-2 bg-white p-6 rounded-lg shadow-md border-t-4 border-orange-500">
+            <h2 class="text-xl font-bold mb-6 text-gray-800">Trip Details</h2>
+            <p class="text-gray-400">Our form inputs will go here next!</p>
+        </div>
+
+        <div class="bg-white p-6 rounded-lg shadow-md border-t-4 border-gray-800">
+            <h2 class="text-xl font-bold mb-6 text-gray-800">Summary</h2>
+            <p class="text-gray-400">Our earnings calculation will go here!</p>
+        </div>
+
     </div>
 @endsection
